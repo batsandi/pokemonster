350e9d81c585c75518618d3a9169443b66c01b47
Batsandi <geol.aleksandar@gmail.com>
2025-03-26 22:15:42 +0200

    import fix

--- pokemonster/celery.py
+++ pokemonster/celery.py
@@ -1,6 +1,6 @@
 import os
 
-from celery import Celery
+from pokemonster.celery import Celery
 
 # Uncomment below if the other line doesn't work
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemonster.settings')
